7b3dd1e0ee02bd6bbb6eb01a65b6c73cf184d9fd
lsj2015-hub <lsj2015@hotmail.com>
2025-02-28 20:56:01 +0800

    first commit - auth completed

---
+++ client/src/state/api.ts
@@ -0,0 +1,91 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
+import { Manager, Tenant } from "../types/prismaTypes";
+import { createNewUserInDatabase } from "../lib/utils";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession();
+      const { idToken } = session.tokens ?? {};
+      if (idToken) {
+        headers.set('Authorization', `Bearer ${idToken}`);
+      }
+      return headers;
+    },
+  }),
+  reducerPath: 'api',
+  tagTypes: ['Managers', 'Tenants'],
+  endpoints: (build) => ({
+    getAuthUser: build.query<User, void>({
+      queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
+        try {
+          const session = await fetchAuthSession();
+          const { idToken } = session.tokens ?? {};
+          const user = await getCurrentUser();
+          const userRole = idToken?.payload['custom:role'] as string;
+
+          const endpoint =
+            userRole === 'manager'
+              ? `/managers/${user.userId}`
+              : `/tenants/${user.userId}`;
+
+          let userDetailsResponse = await fetchWithBQ(endpoint);
+          console.log('userDetailsResponse =>', userDetailsResponse);
+
+          // if user doesn't exist, create new user
+          if (
+            userDetailsResponse.error &&
+            userDetailsResponse.error.status === 404
+          ) {
+            userDetailsResponse = await createNewUserInDatabase(
+              user,
+              idToken,
+              userRole,
+              fetchWithBQ
+            );
+          }
+
+          return {
+            data: {
+              cognitoInfo: { ...user },
+              userInfo: userDetailsResponse.data as Tenant | Manager,
+              userRole,
+            },
+          };
+        } catch (error: any) {
+          return { error: error.manager || 'Could not fetch user data' };
+        }
+      },
+    }),
+
+    updateTenantSettings: build.mutation<
+      Tenant,
+      { cognitoId: string } & Partial<Tenant>
+    >({
+      query: ({ cognitoId, ...updatedTenant }) => ({
+        url: `tenants/${cognitoId}`,
+        method: 'PUT',
+        body: updatedTenant,
+      }),
+      invalidatesTags: (result) => [{ type: 'Tenants', id: result?.id }],
+    }),
+
+    updateManagerSettings: build.mutation<
+      Tenant,
+      { cognitoId: string } & Partial<Tenant>
+    >({
+      query: ({ cognitoId, ...updatedManager }) => ({
+        url: `managers/${cognitoId}`,
+        method: 'PUT',
+        body: updatedManager,
+      }),
+      invalidatesTags: (result) => [{ type: 'Managers', id: result?.id }],
+    }),
+  }),
+});
+
+export const {
+  useGetAuthUserQuery, useUpdateTenantSettingsMutation, useUpdateManagerSettingsMutation
+} = api;
